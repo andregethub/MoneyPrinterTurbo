8322e4129ac33f4115a2a65aa15df06e4ac0900d
andregethub <filmeparaatv@gmail.com>
2026-09-01 22:50:11 -0300

    fix: install natural ManyLingo pacing

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,10 +2,12 @@
 
 from app.services.manylingo import install_video_patch
 from app.services.manylingo_elevenlabs import install_elevenlabs_timing_patch
+from app.services.manylingo_pacing import install_pacing_patch
 from app.services.manylingo_quality import install_quality_patch
 from app.services.manylingo_timing import install_timing_patch
 
 install_quality_patch()
 install_video_patch()
 install_elevenlabs_timing_patch()
+install_pacing_patch()
 install_timing_patch()
